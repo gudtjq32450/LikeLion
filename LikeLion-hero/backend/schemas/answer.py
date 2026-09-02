cb93bc440592be598144f5486228041dc2c0957a
김영웅 <xodrndishfwk@gmail.com>
2026-09-02 15:30:03 +0900

    asdfja

--- LikeLion-hero/backend/schemas/answer.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class AnswerRequest(BaseModel):
-    answer: str = Field(min_length=1, max_length=2000)
-    question: str = Field(default="", max_length=500)
